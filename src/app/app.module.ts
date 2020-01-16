5c2e9257eb50cee64a160ecab0c0538414b5cd27
hatem-jmai <57534253+hatem-jmai@users.noreply.github.com>
2020-01-16 17:58:17 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
