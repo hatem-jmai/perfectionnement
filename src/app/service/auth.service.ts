00c609844859855599f2a27444ad8d95f4310355
Bouzezi <57668229+Bouzezi@users.noreply.github.com>
2020-02-03 23:30:56 +0100

    Add files via upload

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import { coach } from './entity/coach';
-
+import {coach} from './entity/coach';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user:coach;
-  private baseURL = 'http://localhost:3000/coachs';
-  
+private baseURL = 'http://localhost:3000/coachs';
+
   constructor(private http:HttpClient) { }
   
   createcoach(coach: object): Observable<object> {
     return this.http.post(`http://localhost:3000/addcoachs`, coach);
   }
-
-  loginCoach(): Observable<any>{
-    
-    return this.http.get(`${this.baseURL}/login/${this.user.nom}/${this.user.password}`);
-    
+  loginCoach(coach: object): Observable<object>{
+    return this.http.post(`http://localhost:3000/coachs/login`,coach);
   }
 }
--- src/app/service/entity/coach.ts
+++ src/app/service/entity/coach.ts
@@ -7,10 +7,29 @@ date_naiss:string;
 cin:number;
 type :string;
 adresse_mail:string;
+login:string;
 password:string;
 tel:number;
 gouvernorat:string;
 ville:string;
 certificat:string;
 constructor(){}
+
+public get logincoach() : string {
+    return this.login;
+}
+
+public set setlogincoach(v : string) {
+    this.login = v;
+}
+
+public get passwordcoach() : string {
+    return this.password;
+}
+ 
+ public set setpasswordcoach(v : string) {
+     this.password = v;
+ }
+ 
+
 }
