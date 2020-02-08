fc912fad0deff77db2a4e020f128167c0153e981
hatem-jmai <57534253+hatem-jmai@users.noreply.github.com>
2020-02-08 22:49:17 +0100

    home parallex

--- src/app/bodyhome/bodyhome.component.ts
+++ src/app/bodyhome/bodyhome.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-bodyhome',
   templateUrl: './bodyhome.component.html',
   styleUrls: ['./bodyhome.component.css']
 })
 export class BodyhomeComponent implements OnInit {
-
+   // end of jQuery name space
+  
   constructor() { }
 
   ngOnInit() {
+    
+      
+   
+    
   }
-  
-  function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  }; // end of jQuery name space
-  
 }
+
